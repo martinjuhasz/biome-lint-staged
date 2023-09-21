580d8a407ba4a8f81aa3f65d157c59d0a7b96f5d
Martin Juhasz <info@martinjuhasz.de>
2023-09-21 13:02:53 +0200

    feat: inital commit

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = document.getElementById('root') as Element
+
+ReactDOM.createRoot(root).render(<React.StrictMode>Test</React.StrictMode>)
