199f2f97afeb2d6487c9813184937861d4abacea
Martin Juhasz <info@martinjuhasz.de>
2023-09-21 13:04:54 +0200

    feat: add bad format

---
+++ src/ignore/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = document.getElementById('root') as Element
+
+ReactDOM.createRoot(root).render(
+	<React.StrictMode>
+		Test
+	</React.StrictMode>,
+)
